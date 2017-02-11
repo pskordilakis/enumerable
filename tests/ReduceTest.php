faadbf45df89676be9df7ad0477c87fbe903fdd9
Panagiotis Skordilakis <p.skordilakis@gmail.com>
2017-02-11 23:42:19 +0200

    added test

--- tests/ReduceTest.php
+++ tests/ReduceTest.php
@@ -7,7 +7,7 @@ class ReduceTest extends TestCase
     /**
      * @test
      */
-    public function sum()
+    public function sum_with_initial()
     {
         $arr = [1, 2, 3];
 
@@ -16,5 +16,19 @@ class ReduceTest extends TestCase
         }, 0);
 
         $this->assertEquals($sum, 6);
+    }
+
+    /**
+     * @test
+     */
+    public function sum_without_initial()
+    {
+        $arr = [1, 2, 3];
+
+        $sum = \Enumerable\Enumerable::reduce($arr, function ($item, $acc) {
+            return $acc + $item;
+        });
+
+        $this->assertEquals($sum, 7);
     }
 }
